bc98b598dbba096b1dff3c28aa79e96c2735ea72
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-18 09:45:05 +0530

    Create Java Basic Data Types.java

---
+++ Java Basic Data Types.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    int javaIntType(Scanner sc) {
+        // code here
+        int a = sc.nextInt();
+        return a;
+        
+    }
+    
+    String javaStringType(Scanner sc) {
+        // code here
+        String b = sc.next();
+        return b;
+    }
+    
+    float javaFloatType(Scanner sc) {
+        // code here
+        float f = sc.nextFloat();
+        return f;
+    }
+};
