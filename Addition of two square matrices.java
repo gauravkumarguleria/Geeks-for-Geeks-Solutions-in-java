1164452fac443270942c0122ce10ba376198c38e
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-09-01 17:20:58 +0530

    Create Addition of two square matrices.java

---
+++ Addition of two square matrices.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public void  Addition(int[][] grid1, int[][] grid2)
+    {
+        // code here
+        for(int row=0;row<grid1.length;row++){
+            for(int col = 0; col<grid1.length;col++){
+                grid1[row][col] += grid2[row][col];
+            }
+        }
+    }
+}
