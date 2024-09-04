4e1b7cd40ff89b76ad118ef33623c37aeefb5895
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-09-04 15:51:58 +0530

    Create Number of occurrence.java

---
+++ Number of occurrence.java
@@ -0,0 +1,39 @@
+class Solution {
+     public int firstOccurence(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public int lastOccurence(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return right;
+    }
+    int count(int[] nums, int n, int target) {
+        int f1 = firstOccurence(nums, target);
+        int f2 = lastOccurence(nums, target);
+
+        if(f1>f2) return 0;
+        else return (f2 - f1 + 1);
+    }
+}
