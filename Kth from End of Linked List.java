b01c001bc8aac53790926c51ff527dff61bdfc04
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-10-18 10:15:25 +0530

    Update Kth from End of Linked List.java

--- Kth from End of Linked List.java
+++ Kth from End of Linked List.java
@@ -3,24 +3,41 @@ class Solution {
 
     // Function to find the data of kth node from
     // the end of a linked list.
+    // int getKthFromLast(Node head, int k) {
+    //     int size = 0;
+    //     Node curr = head;
+        
+    //     while(curr != null){
+    //         curr = curr.next;
+    //         size++;
+    //     }
+        
+    //     if(k<=0 || k>size){
+    //         return -1;
+    //     }
+        
+    //     curr = head;
+    //     for(int idx = 0; idx< size-k; idx++){
+    //         curr= curr.next;
+    //     }
+        
+        // return curr.data;
+
     int getKthFromLast(Node head, int k) {
-        int size = 0;
-        Node curr = head;
+        Node fast, slow;
+        fast = slow = head;
         
-        while(curr != null){
-            curr = curr.next;
-            size++;
+        for(int idx = 0; idx<k; idx++){
+            if(fast == null) return -1;
+            fast = fast.next;
         }
         
-        if(k<=0 || k>size){
-            return -1;
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
         }
         
-        curr = head;
-        for(int idx = 0; idx< size-k; idx++){
-            curr= curr.next;
-        }
-        
-        return curr.data;
+        return slow.data;
+    }
     }
 }
